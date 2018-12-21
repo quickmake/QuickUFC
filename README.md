1977020f69f84a2051636fa56ce0788ccbbd2138
QuickMake -- 奎克质造 <45538127+quickmake@users.noreply.github.com>
2018-12-21 23:50:35 +0800

    初始添加

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # QuickUFC
+
+1. UFC按键配置: 中文版DCS 改入按键设置,省得需要每一个键去设置.
+2.警告灯服务程序: 用来转发DCS警告灯的状态,并发送给QuickUFC. 需要先运行此程序.
+3.支持DCS-BIOS协议,只需改动一句代码 DCS-BIOS/BIOSConfig.lua  ,   找到并修改如下 BIOS.protocol_io.UDPSender:create({ port = 7777, host = "127.0.0.1" })  
+4.dcs-bios-v0.7.1_QuickUFC.zip 为已经修改好的dcs-bios v0.71的版本,可以直接复制到 C:\Users\你的主机名\Saved Games\DCS\Scripts中. 如果找不到直接把下边复制到地址栏: 
+%USERPROFILE %\Saved Games\DCS\Scripts 
+直达位置,直接粘贴.
+
+具体操作功能测试视频可打开浏览器访问
+
+https://www.bilibili.com/video/av36194432
+
+设置按键部分可以看下面视频的相关部分
+
+https://www.bilibili.com/video/av24322238
+
+或是b站搜索[ 奎克质造 ] 关键字搜索视频.
---
+++ UFC按键配置/QuickUFC_CN.diff.lua
@@ -0,0 +1,277 @@
+local diff = {
+	["keyDiffs"] = {
+		["d1295pnilu1295cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "UFC 1 按键",
+		},
+		["d1296pnilu1296cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "UFC 2 按键",
+		},
+		["d1297pnilu1297cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "UFC 3 按键",
+		},
+		["d1298pnilu1298cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "UFC 4 按键",
+		},
+		["d1299pnilu1299cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "UFC 5 按键",
+		},
+		["d1300pnilu1300cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "UFC 6 按键",
+		},
+		["d1301pnilu1301cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "UFC 7 按键",
+		},
+		["d1302pnilu1302cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "UFC 8 按键",
+		},
+		["d1303pnilu1303cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "UFC 9 按键",
+		},
+		["d1304pnilu1304cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "UFC 0 按键",
+		},
+		["d1305pnilu1305cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "UFC SPC 按键",
+		},
+		["d1306pnilu1306cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "UFC HACK 按键",
+		},
+		["d1307pnilu1307cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "UFC FUNC 按键",
+		},
+		["d1308pnilu1308cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "UFC LTR 按键",
+		},
+		["d1309pnilu1309cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "UFC CLR 按键",
+		},
+		["d1310pnilu1310cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "UFC ENT 按键",
+		},
+		["d1311pnilu1311cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "UFC MK 按键",
+		},
+		["d1312pnilu1312cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+				},
+			},
+			["name"] = "UFC ALT 按键",
+		},
+		["d1313pnilu1313cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN35",
+				},
+			},
+			["name"] = "UFC 主注意灯 按键",
+		},
+		["d1314pnilu1314cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN33",
+				},
+			},
+			["name"] = "UFC STEER UP 摇臂开关",
+		},
+		["d1315pnilu1315cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN34",
+				},
+			},
+			["name"] = "UFC STEER DOWN 摇臂开关",
+		},
+		["d1316pnilu1316cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+			["name"] = "UFC DATA UP 摇臂开关",
+		},
+		["d1317pnilu1317cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "UFC DATA DOWN 摇臂开关",
+		},
+		["d1318pnilu1318cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+			["name"] = "UFC SEL UP 摇臂开关",
+		},
+		["d1319pnilu1319cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "UFC SEL DOWN 摇臂开关",
+		},
+		["d1320pnilu1320cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN31",
+				},
+			},
+			["name"] = "UFC DEPR UP 摇臂开关",
+		},
+		["d1321pnilu1321cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+			["name"] = "UFC DEPR DOWN 摇臂开关",
+		},
+		["d1322pnilu1322cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN30",
+				},
+			},
+			["name"] = "UFC INTEN INCREASE 摇臂开关",
+		},
+		["d1323pnilu1323cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN29",
+				},
+			},
+			["name"] = "UFC INTEN DECREASE 摇臂开关",
+		},
+		["d350pnilu351cdnilvdnilvpnilvunil"] = {
+			["name"] = "武器发射",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+		},
+		["d554pnilu638cdnilvdnilvpnilvunil"] = {
+			["name"] = "机炮扳机",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+		["d562pnilu606cdnilvdnilvpnilvunil"] = {
+			["name"] = "HOTAS 前轮转向按钮",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+		},
+		["d957pnilu958cdnilvdnilvpnilvunil"] = {
+			["name"] = "驾驶杆转配平控制模式",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+		},
+		["dnilp36unilcdnilvdnilvpnilvunil"] = {
+			["name"] = "视角回中",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+		},
+	},
+}
+return diff
